72d2e374dd443fa837a9cc8fdeb2dade7f74cdbf
Christopher Humphries <engleek@gmail.com>
2013-11-07 17:47:24 +0100

    Translated to French.

--- French/definitions.Answers.php
+++ French/definitions.Answers.php
@@ -2,11 +2,11 @@
 // Copyright 2013 Toby Zerner, Simon Zerner
 // This file is part of esoTalk. Please see the included license file for usage information.
 
-// English Definitions for the Answers plugin.
+// French Definitions for the Answers plugin.
 
-$definitions["Answer"] = "Answer";
-$definitions["Answered"] = "Answered";
-$definitions["This post answered my question"] = "This post answered my question";
-$definitions["Answered by %s"] = "Answered by %s";
-$definitions["See post in context"] = "See post in context";
-$definitions["Remove answer"] = "Remove answer";
+$definitions["Answer"] = "Réponse";
+$definitions["Answered"] = "Répondu";
+$definitions["This post answered my question"] = "Ce post répond à ma question";
+$definitions["Answered by %s"] = "Répondu par %s";
+$definitions["See post in context"] = "Voir le contexte du post";
+$definitions["Remove answer"] = "Enlever réponse";
