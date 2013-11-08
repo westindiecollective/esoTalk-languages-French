d84ddc8cadd4f0cc19804a9ac28caaea947fb2d1
Christopher Humphries <engleek@gmail.com>
2013-11-08 11:49:05 +0100

    Translated to French.

--- French/definitions.Views.php
+++ French/definitions.Views.php
@@ -2,8 +2,8 @@
 // Copyright 2013 Toby Zerner, Simon Zerner
 // This file is part of esoTalk. Please see the included license file for usage information.
 
-// English Definitions for the Profiles plugin.
+// French Definitions for the Profiles plugin.
 
-$definitions["gambit.order by views"] = "order by views";
-$definitions["%s view"] = "%s view";
-$definitions["%s views"] = "%s views";
+$definitions["gambit.order by views"] = "trier par vues";
+$definitions["%s view"] = "%s vue";
+$definitions["%s views"] = "%s vues";
