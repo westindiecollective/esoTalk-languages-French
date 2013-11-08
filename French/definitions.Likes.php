4451c59366d91d06c82ed4f4f783194c94805279
Christopher Humphries <engleek@gmail.com>
2013-11-08 11:50:53 +0100

    Meta.

--- French/definitions.Likes.php
+++ French/definitions.Likes.php
@@ -2,7 +2,7 @@
 // Copyright 2013 Toby Zerner, Simon Zerner
 // This file is part of esoTalk. Please see the included license file for usage information.
 
-// English Definitions for the Profiles plugin.
+// French Definitions for the Profiles plugin.
 
 $definitions["Unlike"] = "Je n'aime plus";
 $definitions["Like"] = "J'aime";
