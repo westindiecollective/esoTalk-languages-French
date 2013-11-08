9257266b908772c3610ddeb9c62d8fd46c9c10cf
Christopher Humphries <engleek@gmail.com>
2013-11-08 11:50:18 +0100

    Meta.

--- French/definitions.Debug.php
+++ French/definitions.Debug.php
@@ -2,7 +2,7 @@
 // Copyright 2011 Toby Zerner, Simon Zerner
 // This file is part of esoTalk. Please see the included license file for usage information.
 
-// English Definitions for the Debug plugin.
+// French Definitions for the Debug plugin.
 
 $definitions["MySQL queries"] = "Requêtes MySQL";
 $definitions["Page loaded in %s seconds"] = "Page chargée en %s secondes";
