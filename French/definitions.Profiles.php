75e8816dc34c4cc21a604e2f21373981e8c8f47a
Christopher Humphries <engleek@gmail.com>
2013-11-08 12:44:18 +0100

    Meta.

--- French/definitions.Profiles.php
+++ French/definitions.Profiles.php
@@ -2,7 +2,7 @@
 // Copyright 2013 Toby Zerner, Simon Zerner
 // This file is part of esoTalk. Please see the included license file for usage information.
 
-// English Definitions for the Profiles plugin.
+// French Definitions for the Profiles plugin.
 
 $definitions["About"] = "À propos";
 $definitions["Location"] = "Lieu";
