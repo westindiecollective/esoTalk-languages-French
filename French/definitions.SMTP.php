0e4e9fab6c71f4dd47eb4b7968ea319da89064cd
Christopher Humphries <engleek@gmail.com>
2013-11-08 12:44:47 +0100

    Meta.

--- French/definitions.SMTP.php
+++ French/definitions.SMTP.php
@@ -2,7 +2,7 @@
 // Copyright 2013 Toby Zerner, Simon Zerner
 // This file is part of esoTalk. Please see the included license file for usage information.
 
-// English Definitions for the SMTP plugin.
+// French Definitions for the SMTP plugin.
 
 $definitions["Server"] = "Serveur";
 $definitions["Port"] = "Port";
